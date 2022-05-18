4ffbb421cc5f8b5e643046fab8740c7745996029
李良贤 <liangxian.li@patpat.com>
2022-05-18 18:06:25 +0800

    feat: json转excel

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="home">
-    <HelloWorld :msg="home"></HelloWorld>
+  <div class='home'>
+    <router-link to='/about'>about</router-link><br/>
+    <router-link to='/json-to-excel'>json to excel</router-link>
   </div>
 </template>
 
 <script setup lang='ts'>
-import HelloWorld from '../../components/HelloWorld.vue'
-console.log('home')
-const home = 'home'
 </script>
 
 <style scoped>
-
 </style>
